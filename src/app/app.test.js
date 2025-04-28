7aad2690d24bff91ce0cdb2874bab9fe9240b929
Bella <isabellalimaelima@gmail.com>
2025-04-28 20:01:07 -0300

    adicionando teste

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -13,3 +13,16 @@ describe("Testando o servidor HTTP", () => {
     expect(response.text).toBe("Olá, mundo! Servidor Node.js funcionando!");
   });
 });
+
+describe("Testando o servidor HTTP", () => {
+    it("Deve retornar status 200 e a mensagem correta", async () => {
+      const response = await request(server).get("/");
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toBe("Olá, mundo! Servidor Node.js funcionando!");
+    });
+  
+    it("Deve retornar o cabeçalho Content-Type como text/plain", async () => {
+      const response = await request(server).get("/");
+      expect(response.headers["content-type"]).toBe("text/plain");
+    });
+  });
