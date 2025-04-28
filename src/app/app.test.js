f092e829a506bcd915c03de20c27854908bda1c9
Bella <isabellalimaelima@gmail.com>
2025-04-28 19:52:30 -0300

    criando teste

---
+++ src/app/app.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Olá, mundo! Servidor Node.js funcionando!");
+});
+
+describe("Testando o servidor HTTP", () => {
+  it("Deve retornar status 200 e a mensagem correta", async () => {
+    const response = await request(server).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("Olá, mundo! Servidor Node.js funcionando!");
+  });
+});
