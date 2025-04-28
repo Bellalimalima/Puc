e146579e9176521f54f44f7b89ce7dbf13861b26
Bella <isabellalimaelima@gmail.com>
2025-04-28 19:32:23 -0300

    corrigindo erros

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,12 @@
-// Importando o módulo http do Node.js
 const http = require("http");
 
-// Criando o servidor
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" }); // Cabeçalho da resposta
-  res.end("Olá, mundo! Servidor Node.js funcionando!"); // Resposta
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Olá, mundo! Servidor Node.js funcionando!");
 });
 
 const PORT = 3000;
 
-// Iniciando o servidor
 server.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
